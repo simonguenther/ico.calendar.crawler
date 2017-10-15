13aabbd39ff2b2584fdda16cadb36d8e3e11dbfc
simonguenther <simonpublic@fam-guenther.com>
2017-10-15 03:06:17 +0200

    start it from here

---
+++ test.py
@@ -0,0 +1,10 @@
+from investitin.crawler import ICOCrawler_Investitin
+from icorating.crawler import ICOCrawler_Icorating
+
+icorating = ICOCrawler_Icorating()
+icorating.run()
+
+"""
+crawler = ICOCrawler_Investitin()
+crawler.run()
+"""
